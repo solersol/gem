d1d865e16155de87b51894d4abeb6cd68e7c651e
solersol <ttpage1tk@gmail.com>
2024-08-18 23:27:05 +0300

    Vd

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+
+import { Hero, About, Roadmap2,Roadmap3, About2,About3,About4,Links, NewSection, Join , ComingSoon} from "./sections";
+import VideoBackground from "./components/VideoBackground";
+import VideoBackground2 from "./components/VideoBackground2";
+
+import React, { useState, useEffect } from 'react';
+import Nav from "./components/Nav";
+import Separator from "./components/Separator";
+import SeparatorReverse from "./components/SeparatorReverse";
+
+import StickyFooter from "./sections/StickyFooter";
+import Chart from "./sections/Chart";
+import LoadingScreen from "./components/LoadingScreen"; // Import the loading screen
+
+// Import the background image
+import rdImage from './assets/images/roadmap2.png';
+import main2 from './assets/images/main2.png';
+import main3 from './assets/images/main3.png';
+import bgImage from './assets/images/main.png';
+import mainbg from './assets/images/mainbg.png';
+import linkBg from './assets/images/linkBg.png'
+
+
+
+// import { bg3 } from "./assets/images";
+
+
+
+const App = () => {
+  // const [isLoading, setIsLoading] = useState(true);
+
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setIsLoading(false);
+  //   }, 1000); // Set the loading time to 3 seconds
+
+  //   return () => clearTimeout(timer);
+  // }, []);
+
+  // if (isLoading) {
+  //   return <LoadingScreen />;
+  // }
+return(
+  <main className="relative bg-[#fbe5c4]" >
+    {/* <Nav /> */}
+   
+
+  
+
+
+    {/* <section className="w-full h-screen bg-cover bg-center " style={{ backgroundImage: `url(${main3})` }}><ComingSoon /></section> */}
+   
+    <VideoBackground >
+      <Hero />
+    </ VideoBackground >
+    <Separator />
+
+  
+
+    <section className="w-full h-screen bg-cover bg-center   " style={{ backgroundImage: `url(${bgImage})` }}>
+      <Roadmap2 />
+    </section>
+    <SeparatorReverse />
+    
+    <About4 />
+    <section className="w-full h-full bg-repeat bg-center   " style={{ backgroundImage: `url(${linkBg})` }}>
+    <Links />
+    </section>
+        
+        <Join />
+     
+
+
+    <section className=""><StickyFooter /></section> 
+  </main>
+);
+};
+
+export default App;
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <section id="home" className="w-full h-screen flex flex-col justify-center items-center min-h-screen pb-[32rem] bg-cover bg-center">
+      {/* <h1 className="flex justify-center font-comic  text-[70px] mb-0 text-[#fff]">$F1</h1> */}
+      {/* <h2 className="flex justify-center font-comic  text-[22px] mb-10 mt-0 text-[#dfe2ff]">F1 MEMES</h2> */}
+      <h1 className="flex justify-center font-comic  text-[35px] mt-6 text-center px-4 break-words text-[#000] animate-slide-in-left">CA: 4B9Z8Wrjw8b8DAYdS2x89eedHQ5rVoVkyRFQfpT2pump  </h1>
+
+      <a href="https://pump.fun/4B9Z8Wrjw8b8DAYdS2x89eedHQ5rVoVkyRFQfpT2pump"><button className="animate-bounce mt-6 bg-[#52d591] text-[#fff] rounded-full px-28 py-6 hover:bg-[#f0ff00] border-4 border-black  font-comic  text-[24px] ">
+        BUY $POPS
+      </button></a>
+
+    </section>
+  );
+};
+
+export default Hero;
